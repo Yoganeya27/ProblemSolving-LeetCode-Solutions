37bf657921e2215feed629dc03921fb4642b1fc2
Yoganeya27 <yoganeya.cse@gmail.com>
2026-01-05 14:25:15 +0530

    Time: 262 ms (13.21%) | Memory: 0B (100.00%) - LeetSync

--- MySQL/Easy/620-not-boring-movies/not-boring-movies.sql
+++ MySQL/Easy/620-not-boring-movies/not-boring-movies.sql
@@ -1,3 +1,3 @@
 SELECT * FROM Cinema
-WHERE id%2=1 AND description!="boring" -- In SQL <> symbol can be used to denote !=.
+WHERE id%2=1 AND description!="boring" -- In SQL <> symbol can be used to denote !=
 ORDER BY rating DESC;
