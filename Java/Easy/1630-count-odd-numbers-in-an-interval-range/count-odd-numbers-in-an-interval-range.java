fea308ba3dbe1cff4022f5a105a55c60cb5a4f73
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-07 21:48:36 +0530

    Time: 2428 ms (5.03%) | Memory: 42.3 MB (16.00%) - LeetSync

--- Java/Easy/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ Java/Easy/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -1,7 +1,7 @@
-/*class Solution {
+class Solution {
     public int countOdds(int low, int high) {
         int i,count=0;
-        if(low%2==0){   /* To Prevent TLE(Time Limit Exception).start from the first odd number until high increasing by 2 numbers 
+        if(low%2==0){   // To Prevent TLE(Time Limit Exception).start from the first odd number until high increasing by 2 numbers 
             low++;
         }
         for(i=low;i<=high;i+=2){
@@ -11,10 +11,12 @@
         }
         return count;
     }
-                            (OR)*/
+}
+/*                         (OR)
 class Solution {
     public int countOdds(int low, int high) {
         return (high + 1) / 2 - (low / 2);  // Counts the odd numbers between low and high (inclusive) by taking all odds from 1 to high and subtracting all odds from 1 to low-1.
 
     }
 }
+*/
