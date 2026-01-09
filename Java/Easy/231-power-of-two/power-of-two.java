7fcab9901d2a69678fd7c45260092d7b2d4e1a25
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-09 11:38:55 +0530

    Time: 1 ms (95.55%) | Memory: 42.7 MB (20.14%) - LeetSync

--- Java/Easy/231-power-of-two/power-of-two.java
+++ Java/Easy/231-power-of-two/power-of-two.java
@@ -6,6 +6,20 @@ class Solution {
         while(n%2==0){
             n/=2;
         }
-            return n==1;
+            return n==1; 
     }
 }
+
+/*If a number is divisible by 2 repeatedly (like 2, 4, 8…):
+
+It keeps getting divided by 2 inside the while loop.
+
+Eventually, n becomes 1 (because powers of two eventually reduce to 1).
+
+After the loop, n == 1 → returns true.
+
+If a number is not a power of two (like 3, 6, 12…):
+
+It either never enters the while loop (if it’s odd) or the loop stops at a number greater than 1 that isn’t divisible by 2.
+
+After the loop, n != 1 → returns false.*/
