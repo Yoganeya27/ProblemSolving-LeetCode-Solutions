f8512a7af6e4202652113ef635b708bcca5e2937
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-12 15:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (75.79%) - LeetSync

---
+++ C_Plus_Plus/Easy/66-plus-one/plus-one.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;  
+            }
+            digits[i] = 0;  
+        }
+        
+        
+        vector<int> res(digits.size() + 1, 0);
+        res[0] = 1;  
+        return res;
+    }
+};
