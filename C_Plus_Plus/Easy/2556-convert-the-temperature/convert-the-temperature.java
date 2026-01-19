5c83ee43e027ce06dc9fa6780186c77fc98c8d61
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-19 21:16:30 +0530

    Delete C_Plus_Plus/Easy/2556-convert-the-temperature directory

--- C_Plus_Plus/Easy/2556-convert-the-temperature/convert-the-temperature.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public double[] convertTemperature(double celsius) {
-        double Kelvin=celsius+273.15;
-        double Farenheit=(celsius*1.80)+32.00;
-        return new double[]{Kelvin,Farenheit};
-    }
-}
