8ad076f7463cec250a2ce8461771108439525a08
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-19 21:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (82.99%) - LeetSync

--- C_Plus_Plus/Easy/2556-convert-the-temperature/convert-the-temperature.java
+++ C_Plus_Plus/Easy/2556-convert-the-temperature/convert-the-temperature.java
@@ -1,7 +1,7 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        return new double[]{kelvin, fahrenheit};
+        double Kelvin=celsius+273.15;
+        double Farenheit=(celsius*1.80)+32.00;
+        return new double[]{Kelvin,Farenheit};
     }
 }
