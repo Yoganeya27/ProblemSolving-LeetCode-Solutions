2d7f10561cb561a7895353e91985f958b3ebf43f
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-21 14:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (8.04%) - LeetSync

--- C_Plus_Plus/Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ C_Plus_Plus/Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         return n;
-        
     }
 };
+
+/*
+Sum Of Odd numbers within range  n OddSum=> n^2
+Sum Of Even numbers within range n EvenSum=> n (n+1)
+GCD ==> Difference of EvenSum - OddSum
+    ==> n(n+1)-n*n
+    ==> n*n + n -n*n
+GCD ==> n
+*/
