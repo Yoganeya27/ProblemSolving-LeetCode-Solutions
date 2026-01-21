778c33cea9ce9f4530eba13c17ed1cd772b99259
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-21 11:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.33%) - LeetSync

--- C_Plus_Plus/Easy/2507-number-of-common-factors/number-of-common-factors.cpp
+++ C_Plus_Plus/Easy/2507-number-of-common-factors/number-of-common-factors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int commonFactors(int a, int b) {
         int count=0;
-        int c=max(a,b);
+        int c=max(a,b); // To find the ending range
         for(int i=1;i<=c;i++){
             if(a%i==0 && b%i==0)
             count++;
