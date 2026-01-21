aa52260a22d6d927b076d384456b7c56d3503b4f
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-21 16:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (19.65%) - LeetSync

--- C_Plus_Plus/Easy/3830-find-closest-person/find-closest-person.cpp
+++ C_Plus_Plus/Easy/3830-find-closest-person/find-closest-person.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-       int diff1 = x-z;
-       int diff2 = y-z;
-       if(diff1<0)
-       diff1=-diff1;
-
-       if(diff2<0)
-       diff2=-diff2;
-         if(diff1==diff2) return 0;
-         if(diff1<diff2)
-         return 1;
-         return 2;
+        int diff1 = x-z;
+        int diff2 = y-z;
+        if(diff1<0) diff1=-diff1;
+        if(diff2<0) diff2=-diff2;
+        if(diff1==diff2) return 0;
+        if(diff1<diff2)  return 1;
+        return 2;
     }
 };
+/*                          (OR)
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int diff1 = (x - z) * (x - z);    //gives the squared distance → always positive
+
+        int diff2 = (y - z) * (y - z);   
+        return (diff1 == diff2) ? 0 : (diff1 < diff2 ? 1 : 2); Ternary operator chooses the closest person
+    }
+}; */
