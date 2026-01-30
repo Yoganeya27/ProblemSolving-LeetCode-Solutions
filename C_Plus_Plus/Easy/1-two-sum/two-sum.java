00531c59c59e8d58c30629a7cb6721af160cdfc1
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-30 14:38:25 +0530

    Time: 45 ms (27.76%) | Memory: 47.2 MB (32.20%) - LeetSync

--- C_Plus_Plus/Easy/1-two-sum/two-sum.java
+++ C_Plus_Plus/Easy/1-two-sum/two-sum.java
@@ -11,3 +11,22 @@ class Solution {
         return new int[]{-1, -1};
     }
 }
+   /*                                   (OR)
+   Using HashMap
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // number -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; // what we need to reach target
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i}; // found the pair
+            }
+            map.put(nums[i], i); // store current number for future check
+        }
+
+        return new int[]{-1, -1}; // if no pair found
+    }
+}
+*/
