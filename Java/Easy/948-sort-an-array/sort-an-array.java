d5a924e4f4f0b0036779b1540e9a3c62511478d2
Yoganeya T K <yoganeya.cse@gmail.com>
2026-01-31 10:15:36 +0530

    Time: 21 ms (91.81%) | Memory: 83.2 MB (59.04%) - LeetSync

--- Java/Easy/948-sort-an-array/sort-an-array.java
+++ Java/Easy/948-sort-an-array/sort-an-array.java
@@ -4,4 +4,33 @@ class Solution {
         Arrays.sort(nums);
         return nums;
     } 
+} 
+/*                             (OR)
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+
+        int[] count = new int[max + 1];
+
+        // Count frequency
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        int index = 0;
+
+        // Rebuild sorted array
+        for (int i = 0; i < count.length; i++) {
+            while (count[i]-- > 0) {
+                nums[index++] = i;
+            }
+        }
+
+        return nums;
+    }
 }
+*/
