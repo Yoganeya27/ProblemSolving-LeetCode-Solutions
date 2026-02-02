2b97f31a535b961896f90330714192fe15a911d2
Yoganeya T K <yoganeya.cse@gmail.com>
2026-02-02 14:44:49 +0530

    Time: 1 ms (99.73%) | Memory: 42.9 MB (9.48%) - LeetSync

---
+++ Java/Easy/7-reverse-integer/reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int  num=x;
+        long rev=0;
+        while(num!=0){
+            rev=rev*10+(num%10);
+            num=num/10;
+        }
+       if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+        return 0;
+       }
+        return (int)rev;
+    }
+}
