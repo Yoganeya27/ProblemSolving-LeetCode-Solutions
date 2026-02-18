caf7bc496cb47a3a8cba83144c42bae2ab23c0ec
Yoganeya T K <yoganeya.cse@gmail.com>
2026-02-18 09:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.28%) - LeetSync

--- Java/Easy/70-climbing-stairs/climbing-stairs.java
+++ Java/Easy/70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
-
-        int a = 1, b = 2;
-
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+        int n1=0,n2=1,sum=0;
+        for(int i=1;i<=n;i++){
+            sum=n1+n2;
+            n1=n2;
+            n2=sum;
         }
-
-        return b;
+        return sum;
     }
 }
