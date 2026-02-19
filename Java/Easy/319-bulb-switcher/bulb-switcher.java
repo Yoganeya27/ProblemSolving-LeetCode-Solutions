f69e9b014f221c12c1efeed4f616a5c06a5058c6
Yoganeya T K <yoganeya.cse@gmail.com>
2026-02-19 11:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (86.45%) - LeetSync

--- Java/Easy/319-bulb-switcher/bulb-switcher.java
+++ Java/Easy/319-bulb-switcher/bulb-switcher.java
@@ -7,3 +7,17 @@ class Solution {
         return count;
     }
 }
+
+/* We have n bulbs initially OFF.
+Each bulb is toggled once for every divisor it has.
+If a bulb is toggled even number of times → it becomes OFF.
+If a bulb is toggled odd number of times → it remains ON.
+Only numbers with an odd number of divisors stay ON.
+Only perfect square numbers have an odd number of divisors.
+Therefore, only bulbs at positions:
+1, 4, 9, 16, 25, ... (perfect squares)
+will remain ON after all rounds.
+So the final answer is:
+Count how many perfect square numbers are less than or equal to n.
+In other words:
+Count all i such that i*i <= n.*/
