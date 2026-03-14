2913a5065a35125c298cc44fddbc1cd5eec9c88f
Yoganeya T K <yoganeya.cse@gmail.com>
2026-03-14 15:06:06 +0530

    Time: 1 ms (1.91%) | Memory: 44.6 MB (90.79%) - LeetSync

---
+++ Python/Easy/35-search-insert-position/search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)
+                ans=i;
+            else{
+                if(target>nums[i])
+                ans=i+1;
+            }
+        }
+         return ans;
+    }
+    }
