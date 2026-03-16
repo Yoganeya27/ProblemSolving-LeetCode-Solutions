0e67d6e4aa2b63f1e698330277c4c20f9d6f9120
Yoganeya T K <yoganeya.cse@gmail.com>
2026-03-16 15:36:05 +0530

    Time: 8 ms (16.35%) | Memory: 47.6 MB (17.73%) - LeetSync

--- Python/Easy/268-missing-number/missing-number.java
+++ Python/Easy/268-missing-number/missing-number.java
@@ -7,6 +7,19 @@ class Solution {
                 return i;
             }
         }
-        return nums.length;
+        return nums.length; //// If all positions match, the missing number is nums.length
+    }
+} 
+                                /* OR
+                                class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2; // Sum of numbers from 0 to n
+        int actualSum = 0;
+      for (int num : nums) {  // Sum all elements in the array
+            actualSum += num;
+        }
+        return expectedSum - actualSum; // Missing number = expected sum - actual sum
     }
 }
+*/
