5f8421dfa484e7eee8a7556479800e3d54dfef32
Yoganeya T K <yoganeya.cse@gmail.com>
2026-03-16 14:57:09 +0530

    Time: 25 ms (13.35%) | Memory: 76.8 MB (83.80%) - LeetSync

--- Python/Easy/217-contains-duplicate/contains-duplicate.java
+++ Python/Easy/217-contains-duplicate/contains-duplicate.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
+        Arrays.sort(nums);                         /*Sorting groups same numbers together,so duplicates become easy to detect in one pass.*/
+                                                     
         for(int i=1;i<nums.length;i++){
-             if(nums[i]==nums[i-1])
+             if(nums[i]==nums[i-1])     
               return true;
         }
         return false;
