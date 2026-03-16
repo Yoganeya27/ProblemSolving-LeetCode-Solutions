7f795e526ddc722058e65f1034d8639c87587e9b
Yoganeya T K <yoganeya.cse@gmail.com>
2026-03-16 10:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (99.95%) - LeetSync

--- Python/Easy/66-plus-one/plus-one.java
+++ Python/Easy/66-plus-one/plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-        for(int i = digits.length - 1; i >= 0; i--){
+        for(int i = digits.length - 1; i >= 0; i--){ //When adding 1 to a number, we start from the rightmost digit.
             if (digits[i]<9){
                 digits[i]++;
                 return digits;
