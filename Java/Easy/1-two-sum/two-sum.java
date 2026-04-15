f11797673cb1c78f9906751c488f563b76538684
Yoganeya T K <yoganeya.cse@gmail.com>
2026-04-15 22:57:31 +0530

    Time: 45 ms (26.05%) | Memory: 47.1 MB (62.37%) - LeetSync

--- Java/Easy/1-two-sum/two-sum.java
+++ Java/Easy/1-two-sum/two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
+
         for (int i = 0; i < nums.length; i++) { //Outer loop picks one number nums[i].
             for (int j = i + 1; j < nums.length; j++) { //Inner loop checks every number after i (nums[j]) so we don’t repeat pairs.
                 if (nums[i] + nums[j] == target) {
@@ -8,7 +9,7 @@ class Solution {
                 }
             }
         }
-        return new int[]{-1, -1};
+        return new int[]{-1, -1}; // if no pair found
     }
 }
    /*                                   (OR)
@@ -18,6 +19,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>(); // number -> index
 
+
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i]; // what we need to reach target
             if (map.containsKey(complement)) {
@@ -25,6 +27,7 @@ class Solution {
             }
             map.put(nums[i], i); // store current number for future check
         }
+
 
         return new int[]{-1, -1}; // if no pair found
     }
