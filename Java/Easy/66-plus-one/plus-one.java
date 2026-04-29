88dee38580ec2f3f7bf4fa5c03f31d72d2aa3637
Yoganeya T K <yoganeya.cse@gmail.com>
2026-04-30 00:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (94.88%) - LeetSync

--- Java/Easy/66-plus-one/plus-one.java
+++ Java/Easy/66-plus-one/plus-one.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+        
+        for(int i = digits.length - 1; i >= 0; i--){ //When adding 1 to a number, we start from the rightmost digit.
+            if (digits[i]<9){
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
+            digits[i]=0; // To replace trailing 9 with 0;
         }
-        int[] res = new int[digits.length + 1];
-        res[0] = 1;
-        return res;
+  int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
